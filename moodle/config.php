4dcd4186044e42b24c9f1c2bf874d72fe7c8923b
POMMOUYLANG <mouylangpom@gamail.com>
2025-10-23 14:21:03 +0700

    updated

--- moodle/config.php
+++ moodle/config.php
@@ -3,6 +3,9 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
+$CFG->wwwroot   = 'http://rtc-bb.camai.kh/moodle';
+$CFG->dataroot  = '/var/www/moodledata';
+
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'moodle-db';
@@ -10,16 +13,6 @@ $CFG->dbname    = 'moodle_lms';
 $CFG->dbuser    = 'root';
 $CFG->dbpass    = 'Root@123';
 $CFG->prefix    = 'mdl_';
-$CFG->dboptions = [
-    'dbpersist' => 0,
-    'dbport' => 3306,
-    'dbsocket' => '',
-    'dbcollation' => 'utf8mb4_unicode_ci',
-];
-
-$CFG->wwwroot   = 'http://rtc-bb.camai.kh/moodle';
-$CFG->dataroot  = '/var/www/moodledata';
-$CFG->admin     = 'admin';
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
