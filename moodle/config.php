69de917ae5c05d67a5783354327f3f7f94fa4882
POMMOUYLANG <mouylangpom@gamail.com>
2025-10-27 09:32:52 +0700

    updated config

--- moodle/config.php
+++ moodle/config.php
@@ -10,11 +10,13 @@ $CFG->dbname    = 'moodle_lms';
 $CFG->dbuser    = 'moodle';
 $CFG->dbpass    = 'Moodle@123';      
 // $CFG->wwwroot   = 'http://rtc-bb.camai.kh/moodle';
-$CFG->wwwroot   = 'http://moodle.rtc-bb.camai.kh';
-$CFG->dataroot  = '/var/moodledata';
+$CFG->wwwroot   = 'https://moodle.rtc-bb.camai.kh';
+$CFG->dataroot  = '/var/www/moodledata';
+
 $CFG->admin     = 'admin';
 
 $CFG->prefix    = 'mdl_';
+
 $CFG->directorypermissions = 0777;
 
 require_once(__DIR__ . '/lib/setup.php');
